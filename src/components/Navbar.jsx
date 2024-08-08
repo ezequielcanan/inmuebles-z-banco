9da2d5c02c1f867d5d8e12d210e25dee7009255d
Ezequiel Canan <ezequielcanan@gmail.com>
2024-08-08 11:35:05 -0300

    Tax by month

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Link, NavLink } from "react-router-dom"
 import { MdAccountBalanceWallet, MdAttachMoney, MdConstruction, MdMoney, MdNotifications, MdOutlineMenu } from "react-icons/md"
 import { FaHelmetSafety } from "react-icons/fa6"
-import { FaCertificate, FaChevronDown, FaMoneyCheckAlt, FaTicketAlt, FaUser, FaUserAlt } from "react-icons/fa"
+import { FaCertificate, FaChevronDown, FaMoneyCheckAlt, FaPercentage, FaTicketAlt, FaUser, FaUserAlt } from "react-icons/fa"
 import { HiOutlineDocument } from "react-icons/hi"
 import { LuLogOut } from "react-icons/lu"
 import { GiMoneyStack } from "react-icons/gi"
@@ -59,6 +59,9 @@ const Navbar = ({ user, setUser }) => {
             <li className={`${"block w-full h-full md:w-auto"} xl:block w-full text-xl`}>
               <NavLink to={"/suppliers"} className={({ isActive }) => `duration-500 py-3 px-3 flex w-full gap-x-[20px] items-center ${isActive ? "bg-secondary text-black" : "bg-inherit text-secondary"}`} onClick={() => setNav(false)}><FaHelmetSafety /> Proveedores</NavLink>
             </li>
+            <li className={`${"block w-full h-full md:w-auto"} xl:block w-full text-xl`}>
+              <NavLink to={"/tax"} className={({ isActive }) => `duration-500 py-3 px-3 flex w-full gap-x-[20px] items-center ${isActive ? "bg-secondary text-black" : "bg-inherit text-secondary"}`} onClick={() => setNav(false)}><FaPercentage /> IIBB</NavLink>
+            </li>
           </ul>
         </div>
         {user ? <div className="xl:self-start">
@@ -79,6 +82,9 @@ const Navbar = ({ user, setUser }) => {
           </div>
           <div className={`xl:block w-full text-xl`}>
             <NavLink to={"/suppliers"} className={({ isActive }) => `duration-500 py-3 px-3 flex w-full gap-x-[20px] items-center ${isActive ? "bg-secondary text-textColor" : "text-secondary bg-inherit"}`} onClick={() => setNav(false)}><FaHelmetSafety /> Proveedores</NavLink>
+          </div>
+          <div className={`xl:block w-full text-xl`}>
+            <NavLink to={"/tax"} className={({ isActive }) => `duration-500 py-3 px-3 flex w-full gap-x-[20px] items-center ${isActive ? "bg-secondary text-textColor" : "text-secondary bg-inherit"}`} onClick={() => setNav(false)}><FaPercentage /> IIBB</NavLink>
           </div>
           <ul className="flex flex-col gap-x-[30px] gap-y-[20px] rounded-md text-black py-6 text-2xl">
             {user ? <li className="pb-2">
---
+++ src/pages/Tax.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react"
+import { BounceLoader } from "react-spinners"
+import Title from "../components/Title"
+import Main from "../containers/Main"
+import customAxios from "../config/axios.config"
+import { Link } from "react-router-dom"
+import Button from "../components/Button"
+import { FaFileExcel, FaPlus, FaTrash } from "react-icons/fa"
+import Section from "../containers/Section"
+import Input from "../components/FormInput/Input"
+import { BsCheck } from "react-icons/bs"
+import Fields from "../components/Fields"
+import { useForm } from "react-hook-form"
+import Form from "../components/Form"
+
+const Tax = () => {
+  const [taxes, setTaxes] = useState(false)
+  const [reload, setReload] = useState(false)
+  const {register, handleSubmit, setFocus, reset} = useForm()
+  const fields = [
+    {name: "name", text: "Servicio:", labelClassName: "!text-lg", className: "!text-lg !w-full", containerClassName: "max-w-full", component: Input},
+    {name: "code", text: "N° de cuenta:", labelClassName: "!text-lg", className: "!text-lg !w-full", containerClassName: "max-w-full", component: Input},
+  ]
+
+
+  useEffect(() => {
+    customAxios.get("/service").then(res => {
+      setServices(res?.data?.payload || [])
+    })
+  }, [reload])
+
+  const onSubmit = handleSubmit(async data => {
+    await customAxios.post("/service", data)
+    reset()
+    setReload(!reload)
+  })
+
+  const onDeleteService = async (id) => {
+    await customAxios.delete(`/service/${id}`)
+    setReload(!reload)
+  }
+
+  return (
+    <Main className={"flex flex-col gap-y-[70px]"} paddings>
+      <Section>
+        <Title className={"text-center md:text-start"}>
+          Servicios
+        </Title>
+      </Section>
+      <section className="grid lg:grid-cols-4 sm:grid-cols-2 gap-6">
+        {services ? (
+          <>
+            {services.length ? (
+              services.map((service, i) => {
+                return <div key={"c"+i} className="bg-teal-500 shadow-[10px_10px_15px_0px_#2226] border-primary flex flex-col justify-between w-full gap-y-4 py-6 px-6 text-black duration-300">
+                <h3 className="text-3xl">{service?.name}: {service?.code}</h3>
+                <h4 className="text-xl">Cantidad de movimientos: {service?.movements}</h4>
+                <div className="flex items-center text-2xl gap-x-4">
+                  <a href={`${import.meta.env.VITE_REACT_API_URL}/api/movement/excel/service/${service?._id}` }>
+                    <Button style="icon" className={"!bg-third text-white rounded-md duration-300 hover:scale-90"}>
+                      <FaFileExcel/>
+                    </Button>
+                  </a>
+                  {!service?.movements ? <Button style="icon" className={"!bg-red-600 text-white rounded-md duration-300 hover:scale-90"} onClick={() => onDeleteService(service?._id)}>
+                    <FaTrash/>
+                  </Button> : null}
+                </div>
+              </div>
+              })
+            ) : (
+              null
+            )}
+            <div>
+              <Form className={"bg-third p-4 text-white"}>
+                <Fields fields={fields} onSubmit={onSubmit} register={register} setFocus={setFocus}/>
+                <Button className={"!text-lg items-center justify-center !p-2"} onClick={e => (e.preventDefault(), onSubmit())}>Agregar Servicio <FaPlus/></Button>
+              </Form>
+            </div>
+          </>
+        ) : <BounceLoader />}
+      </section>
+    </Main>
+  )
+}
+
+export default Tax
